afbe8fb4007d5bd152287dddf57041bb48c4d78f
AntonioBotis <botisantonio@gmail.com>
2025-10-16 16:30:55 +0300

    first commit

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,8 @@
+export default  function TabButton({children,isSelected,...props})
+{
+    function handleClick()
+    {
+        console.log('hi');
+    }
+    return <li><button className={isSelected? 'active':undefined} {...props}>{children}</button></li>;
+}
